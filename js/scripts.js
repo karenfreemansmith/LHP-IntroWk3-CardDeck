301220feb69b80189702a90ec7ce437841b6e151
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-12 20:30:06 -0700

    print out list of all the cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+//alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+
+var suits=["diamonds", "clubs", "hearts", "spades"];
+var cardValues=["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
+
+
+suits.forEach(function(suit) {
+  cardValues.forEach(function(card) {
+    $("#showCards").append("<li>" + card + " of " + suit + "</li>");
+  });
+});
